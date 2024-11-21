92e6b798c785cecae4135ca87cd06c22142ce401
47510 <47510@SMLLTUDR00820>
2024-11-21 11:38:51 +0530

    created form controls

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -1,11 +1,25 @@
-﻿namespace LearnBlazor.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public bool IsActive { get; set; }
+        [Range(1,1000)]
         public double Price { get; set; }
         public IEnumerable<Product_Property> ProductProperties { get; set; }
+        public Category Category { get; set; }
+        public DateTime? AvailableAfter { get; set; } = DateTime.Now;
+
+    }
+
+    public enum Category { 
+        Starter,
+        Dessert,
+        Dinner
     }
 }
