c6abcc1c3387a9717e422aeb6f2ff81a65cb5962
47510 <47510@SMLLTUDR00820>
2024-11-21 09:58:34 +0530

    added blazor life cycle

---
+++ LearnBlazor/HelperExtension/IJSRuntimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.HelperExtension
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jsRuntime,string message) {
+            await jsRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+        public static async Task ToastrWarning(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "warning", message);
+        }
+        public static async Task ToastrInfo(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "info", message);
+        }
+    }
+}
---
+++ LearnBlazor/wwwroot/Js/Common.js
@@ -0,0 +1,49 @@
+﻿
+
+//window.showToastr = function (type, message) {
+//    if (type == "success") {
+//        toastr.success(message)
+//    }
+//    if (type == "error") {
+//        toastr.error(message)
+//    }
+//    if (type == "warning") {
+//        toastr.warning(message)
+//    }
+//};
+
+
+function showToastr(type, message) {
+    if (type == "success") {
+        toastr.success(message)
+    }
+    if (type == "error") {
+        toastr.error(message)
+    }
+    if (type == "warning") {
+        toastr.warning(message)
+    }
+    if (type == "info") {
+        toastr.info(message);
+    }
+};
+
+
+function showSweetAlert(type,message) {
+    if (type == "success") {
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+    if (type == "error") {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: message
+        });
+    }
+}
